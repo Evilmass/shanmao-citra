8d1e7b597f4a49229d6e18fb53029b88a2ecaed0
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-07-15 21:47:27 +0800

    sdl2

--- .ci/windows-msvc/deps.sh
+++ .ci/windows-msvc/deps.sh
@@ -10,7 +10,7 @@ mv ./buildcache/bin/buildcache.exe "/c/ProgramData/chocolatey/bin"
 rm -rf ./buildcache/
 
 # Download and extract SDL2
-SDL2_VER="2.0.14"
+SDL2_VER="2.32.8"
 SDL2_URL="https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VER}/SDL2-devel-${SDL2_VER}-VC.zip"
 wget -O SDL2.zip "$SDL2_URL"
 7z x SDL2.zip -oSDL2-tmp
